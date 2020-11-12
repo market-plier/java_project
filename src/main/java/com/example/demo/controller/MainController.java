7544e187a9fb1bf680965dddf57d9b81ae6628db
Gleb <maksimenkogleb@gmail.com>
2020-11-12 19:53:44 +0200

    first commit

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,78 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    private static List<Person> persons = new ArrayList<Person>();
+
+    static {
+        persons.add(new Person("Bill", "Gates"));
+        persons.add(new Person("Steve", "Jobs"));
+    }
+
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        String message = "Hello Spring Boot + JSP";
+
+        model.addAttribute("message", message);
+
+        return "index";
+    }
+
+    @RequestMapping(value = { "/personList" }, method = RequestMethod.GET)
+    public String viewPersonList(Model model) throws ClassNotFoundException, SQLException {
+        System.out.println("-------- Oracle JDBC Connection Testing ------");
+
+        try {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("Where is your Oracle JDBC Driver?");
+            e.printStackTrace();
+            throw e;
+
+        }
+
+        System.out.println("Oracle JDBC Driver Registered!");
+
+        Connection connection = null;
+
+        try {
+
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:xe", "system", "1234");
+
+        } catch (SQLException e) {
+
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            throw e;
+
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+        model.addAttribute("persons", persons);
+
+        return "personList";
+    }
+
+}
